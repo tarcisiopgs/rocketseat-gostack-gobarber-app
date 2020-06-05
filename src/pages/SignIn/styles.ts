2f56ca60975c4a61f2f930f81a54c3574a8c3d19
Tarcísio Pedro <tarcisiopgs@gmail.com>
2020-06-04 21:18:20 -0300

    initial commit

---
+++ src/pages/SignIn/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+  padding: 0 30px;
+`;
+
+export const Title = styled.Text`
+  font-family: 'RobotoSlab-Medium';
+  color: #f4ede8;
+  font-size: 24px;
+  margin: 64px 0 24px;
+`;
